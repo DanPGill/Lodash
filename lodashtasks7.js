81fb1fe3e211539831bf932c256812046137b18a
Dan <danpgill@gmail.com>
2017-04-03 14:59:47 +0100

    Removed redundancy from lodashtasks7.js

--- lodashtasks7.js
+++ lodashtasks7.js
@@ -3,11 +3,6 @@ var gillFamily = [{name: "john", age: 20},
 {name: "debbie", age: 55},
 {name: "dan", age: 25},
 {name: "robin", age: 60}];
-var gillFamily = [{name: "john", age: 20},
-{name: "richard", age: 27},
-{name: "debbie", age: 55},
-{name: "dan", age: 25},
-{name: "robin", age: 60}];
 var underFifty = _.filter(gillFamily, function(age){
 	return age.age < 50;
 }
