8f56a44c387d55a27380bd66d51d744630a6f621
Dan <danpgill@gmail.com>
2017-04-04 11:34:41 +0100

    Added lodash tasks 16

---
+++ lodashtasks16.js
@@ -0,0 +1,9 @@
+var gillFamily = [{name: "john", age: 20},
+{name: "richard", age: 27},
+{name: "debbie", age: 55},
+{name: "dan", age: 25},
+{name: "robin", age: 60}];
+var capitals = _.map(gillFamily, function(c){
+	return _.capitalize(c.name);
+});
+console.log(capitals);
