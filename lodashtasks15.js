816a340556475e72e6a2f3c4c723c627499a1b6e
Dan <danpgill@gmail.com>
2017-04-04 11:11:52 +0100

    lodash tasks 15 added

---
+++ lodashtasks15.js
@@ -0,0 +1,11 @@
+var gillFamily = [{name: "john", age: 20},
+{name: "richard", age: 27},
+{name: "debbie", age: 55},
+{name: "dan", age: 25},
+{name: "robin", age: 60}];
+var aInName = _.filter(gillFamily, function(a){
+	if (a.name.indexOf("a") !== -1){
+		return a.name;
+	}
+});
+console.log(aInName);
