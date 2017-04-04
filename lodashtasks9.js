d2b811524de9481ccb94fdcbdb57670c0a9b06aa
Dan <danpgill@gmail.com>
2017-04-04 18:43:13 +0100

    Ammended lodash tasks 9

--- lodashtasks9.js
+++ lodashtasks9.js
@@ -3,20 +3,19 @@ var gillFamily = [{name: "john", age: 20},
 {name: "debbie", age: 55},
 {name: "dan", age: 25},
 {name: "robin", age: 60}];
-var html = "<script type =\"text/template\" id=\"grid-template\">\n" +
-"<table>\n" +
+
+var tableContents = _.map(gillFamily, function(t){
+  return "<tr>\n" + "<td>" + t.name + "</td>\n" +
+  "<td>" + t.age + "</td>\n" + "</tr>\n";
+});
+
+var html = "<table>\n" +
 "<tbody>\n" +
 "<tr> \n" +
 "<th>Name</th> \n" +
 "<th>Age</th> \n" +
-"</tr> \n" +
-"<% forEach_.(gillFamily, function(value){ %>\n" +
-"<tr> \n" +
-"<td><%-name%></td> \n" +
-"<td><%-age%></td> \n" +
-"</tr> \n" +
-"<%})%>\n" +
+"</tr>\n" +
+tableContents +
 "</tbody> \n" +
-"</table> \n" +
-"</script>"
+"</table> \n"
 console.log(html);
